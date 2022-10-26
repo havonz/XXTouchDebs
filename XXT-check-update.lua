f1f5f9819f362f2283e7ed9fa8c27107962cb40a
苏泽 <havonz@me.com>
2022-10-27 00:56:18 +0800

    Mac 版取色器启动不跳前台问题解决，支持 Retina

--- XXT-check-update.lua
+++ XXT-check-update.lua
@@ -2,9 +2,9 @@
 return [[
 {
 	"package-id": "com.1func.xxtouch.bigboss",
-	"latest": "1.3-4",
-	"description": "[2022-09-29]\n1.3-4 Changes:\nFixed App crash on iOS11\n1.3-4 改动如下：\n修正 App 在 iOS11 崩溃的问题",
+	"latest": "1.3-5",
+	"description": "[2022-10-25]\n1.3-5 Changes:\nFixed an issue where the remote CC page could not scan the device\nFixed sys.log slow\nFixed an issue with daemon.lua using xxtouch.post to return an incorrect value\nAdd custom OpenAPI features\n1.3-5 改动如下：\n修正远程管理中控页面扫不到设备的问题\n修正 sys.log 耗时长的问题\n修正 daemon.lua 中 xxtouch.post 返回值不正确的问题\n增加自定义 OpenAPI",
 	"cydia-url": "cydia:\/\/package\/com.1func.xxtouch.bigboss",
-	"url": "http:\/\/apt.thebigboss.org\/repofiles\/cydia\/debs2.0\/xxtouch_1.3-4.deb"
+	"url": "http:\/\/apt.thebigboss.org\/repofiles\/cydia\/debs2.0\/xxtouch_1.3-5.deb"
 }
 ]]
