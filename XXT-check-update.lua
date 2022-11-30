6deb2517665a695f440b3634b18e3bbe650e897c
havonz <havonz@me.com>
2022-11-30 17:41:40 +0800

    更新 1.3-7

--- XXT-check-update.lua
+++ XXT-check-update.lua
@@ -1,9 +1,16 @@
-return [[
-{
-	"package-id": "com.1func.xxtouch.bigboss",
-	"latest": "1.3-6",
-	"description": "[2022-11-14]\n1.3-6 Changes:\nFixed an issue where screen.image(0,x,x,x) was misjudged as screen.image()\n1.3-6 改动如下：\n修正 screen.image(0,x,x,x) 会被误判为 screen.image() 的问题",
-	"cydia-url": "cydia:\/\/package\/com.1func.xxtouch.bigboss",
-	"url": "http:\/\/apt.thebigboss.org\/repofiles\/cydia\/debs2.0\/xxtouch_1.3-6.deb"
+return json.encode{
+	['package-id'] = 'com.1func.xxtouch.bigboss',
+	['latest'] = '1.3-7',
+	['cydia-url'] = 'cydia://package/com.1func.xxtouch.bigboss',
+	['url'] = 'https://github.com/havonz/XXTouchDebs/raw/master/com.1func.xxtouch.bigboss_1.3-7_iphoneos-arm.deb',
+	['description'] = [[
+[2022-11-30]
+1.3-7 Changes:
+Fixed iOS 11 RGB10 device screenshot corruption issue 
+Fixed iOS 13 not working on A12 + devices 
+Fixed clipboard permission issues in iOS 15
+1.3-7 改动如下：
+修正 iOS 11 广色域屏设备取色截图不正常问题
+修正 A12 以上设备 iOS 13 插件无法被加载的问题
+修正 iOS 15 剪贴板权限的问题]],
 }
-]]
