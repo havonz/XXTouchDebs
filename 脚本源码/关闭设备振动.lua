fcc4873f1cc0fa5416ecff1ff321e2b0fd859d00
havonz <havonz@me.com>
2025-11-22 22:31:59 +0800

    fix: 优化振动设置逻辑，增加对系统killall函数的支持

--- 脚本源码/关闭设备振动.lua
+++ 脚本源码/关闭设备振动.lua
@@ -1,5 +1,9 @@
 -- 设置 -- 辅助功能 -- 触控 -- 振动
-t = plist.read('/private/var/mobile/Library/Preferences/com.apple.Accessibility.plist')
+local t = plist.read('/private/var/mobile/Library/Preferences/com.apple.Accessibility.plist') or {}
 t.VibrationDisabled = 1
 plist.write('/private/var/mobile/Library/Preferences/com.apple.Accessibility.plist', t)
-os.execute('killall -9 mediaserverd')
+if type(sys.killall) == 'function' then
+    sys.killall(9, 'mediaserverd')
+else
+    os.execute('killall -9 mediaserverd')
+end
