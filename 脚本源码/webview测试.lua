86f0a1fa904d77cfb72def3deb17bdecc1da53bd
havonz <havonz@me.com>
2025-11-22 18:06:37 +0800

    fix: 更新viewport设置以禁用用户缩放防止获得输入焦点时页面缩放导致崩溃

--- 脚本源码/webview测试.lua
+++ 脚本源码/webview测试.lua
@@ -1,5 +1,5 @@
 local html = [=[
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 <!DOCTYPE html>
 <html>
     <head>
